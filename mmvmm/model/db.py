531433e9e1ab3a8a9614bf4028c5aacf08748a8d
marcsello <punkosdmarcell@rocketmail.com>
2020-08-13 10:17:23 +0200

    Added model

---
+++ mmvmm/model/db.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+Base = declarative_base()
+engine = create_engine('sqlite://', echo=True)
+Session = scoped_session(sessionmaker(bind=engine))
