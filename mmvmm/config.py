9d5744106f1c40bedbca730585540093ccc9590f
marcsello <punkosdmarcell@rocketmail.com>
2020-10-31 03:24:23 +0100

    Updated default path of the sqlite db

--- mmvmm/config.py
+++ mmvmm/config.py
@@ -9,4 +9,4 @@ class Config:
     QMP_SOCKETS_DIR = os.path.join(os.environ.get("SOCKET_DIR", "/run/mmvmm"), 'internal')
     QEMU_PATH = os.environ.get("QEMU_PATH", "/usr/bin/qemu-system-x86_64")
     IP_PATH = os.environ.get("IP_PATH", "/sbin/ip")
-    DATABASE_URI = os.environ.get("DATABASE_URI", "sqlite:////tmp/mmvmm.db")
+    DATABASE_URI = os.environ.get("DATABASE_URI", "sqlite:////var/lib/mmvmm/mmvmm.db")
